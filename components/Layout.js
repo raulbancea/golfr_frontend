e1f4f38344afb59c035c0c5a74e0c242c52d3920
Raul <raulbancea@yahoo.com>
2024-09-10 13:28:13 +0300

    Golfers score page

--- components/Layout.js
+++ components/Layout.js
@@ -1,32 +1,44 @@
 import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import { getUsername } from '../lib/userAuth'
+import { getUserId } from '../lib/userAuth'
 
-const Layout = ({ children }) => {
-  const [ username, setUsername ] = useState('')
+const Layout = ({ children, home }) => {
+  const [username, setUsername] = useState('')
   useEffect(() => setUsername(getUsername()), [])
+
+  const [id, setUserId] = useState('')
+  useEffect(() => setUserId(getUserId()), [])
 
   return (
     <>
       <header className="flex flex-row w-full px-10 py-2 shadow">
         <span className="h-8 items-center space-x-6">
           <span className="text-2xl">Golfr 🏌️</span>
-          <span className="text-xl">Home</span>
+          <span className="text-xl">
+            {home ? (
+              <span className="text-xl">Home</span>
+            ) : (
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            )}
+          </span>
         </span>
         <span className="h-8 items-center text-xl ml-auto">
-          {username}
+          <Link href={`/golfers/${id}`}>
+            <a>{username}</a>
+          </Link>
           <span className="text-sm">
             {' ('}
             <Link href={'/logout'}>
-              <a className="underline text-blue-700">logout</a>
+              <a>Logout</a>
             </Link>
             {')'}
           </span>
         </span>
       </header>
-      <div className="px-10 py-2">
-        {children}
-      </div>
+      <main>{children}</main>
     </>
   )
 }
